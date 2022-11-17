62aedee5362f9382317bea3cc563983e8ae219d3
Charnaye Grier <charnayegrier@gmail.com>
2022-11-16 21:26:35 -0500

    changed h3s to links for redirects

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-const hello = alert("Hello");
+const clickHome = () => {
+    document.getElementsByClassName("Home").onclick = location.assign('https://www.javascripttutorial.net/javascript-bom/javascript-redirect/')
+}
+// clickHome()
