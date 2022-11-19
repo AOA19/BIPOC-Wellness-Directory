73aaf1d3c8042abc30cb183440a30475ab070c8d
Deli B <delicia.bills@gmail.com>
2022-11-18 16:30:04 -0800

    feat: add media queries

--- bipoc-wellness-directory/src/App.js
+++ bipoc-wellness-directory/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import './App.css';
+import './App.css';
 import Navbar from './components/Navbar';
 import Homepage from './pages/Homepage/Homepage';
 import About from './pages/About/About';
