0a268561b1f843f5c585d67564cf405196b7fc59
A0A <66294921+AOA19@users.noreply.github.com>
2022-11-18 23:41:05 -0500

    Updated footer

--- bipoc-wellness-directory/src/components/Footer.js
+++ bipoc-wellness-directory/src/components/Footer.js
@@ -3,10 +3,14 @@ import './Footer.css'
 
 function Footer() {
   return (
-    <div className='footer'>
-      <b>Designed and Built by <br />Alexandra A, Bola C, Charnaye G, Delicia B, Jeneice-Demari G, Joan W, Sonia A, & Zoe M</b>
+    <div className="footer">
+      <b>
+        Designed and Built by <br />
+        Alexandra A, Alexandria “Bola” C, Charnaye G, Delicia B, Jeneice-Demari
+        G, Joan W, Michelle K, Sonia A, & Zoe M
+      </b>
     </div>
-  )
+  );
 }
 
 export default Footer
