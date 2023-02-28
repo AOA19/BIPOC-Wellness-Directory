f034087e9dc9cd4b31027ede6a307d20fb318487
A0A <66294921+AOA19@users.noreply.github.com>
2023-02-28 00:00:19 -0500

    Fixed broken link for sign up button

--- bipoc-wellness-directory/src/pages/SignUp/SignUp.js
+++ bipoc-wellness-directory/src/pages/SignUp/SignUp.js
@@ -7,27 +7,27 @@ function SignUp() {
     <div>
       {/* <!------------------------ SIGN UP FORM ----------------------> */}
       <form id="form">
-        <h2 class="form-title">Create an Account</h2>
+        <h2 className="form-title">Create an Account</h2>
         {/* <!-- Full Name --> */}
-        <label for="fname" class="form-label">
+        <label for="fname" className="form-label">
           Full Name
         </label>
         <input type="text" class="form-input" id="fname" required />
         {/* <!-- Email --> */}
-        <label for="email" class="form-label">
+        <label for="email" className="form-label">
           Email
         </label>
-        <input type="email" class="form-input" id="email" required />
+        <input type="email" className="form-input" id="email" required />
         {/* <!-- Password --> */}
-        <label for="password" class="form-label">
+        <label for="password" className="form-label">
           Password
         </label>
-        <input type="password" class="form-input" id="password" required />
+        <input type="password" className="form-input" id="password" required />
         {/* <!-- Dropdown choices --> */}
-        <label for="dropdown" class="form-label">
+        <label for="dropdown" className="form-label">
           Member or Provider?
         </label>
-        <select name="drowdown" class="form-input" required>
+        <select name="drowdown" className="form-input" required>
           <option disabled selected>
             Please select one
           </option>
@@ -35,10 +35,13 @@ function SignUp() {
           <option value="provider">Provider</option>
         </select>
         {/* <!-- Sign up Button --> */}
-        <div class="btn-container">
-          <button type="submit" class="form-btn">
+        <div className="btn-container">
+          {/* <button type="submit" class="form-btn">
             Sign Up
-          </button>
+          </button> */}
+          <Link to="/" className="form-btn">
+            Sign Up
+          </Link>
           <Link to="/login">
             <p>
               Already have an account? <span>Login</span>
